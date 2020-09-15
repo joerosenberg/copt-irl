Added functions for masking in the pointer transformer, fixed pointer transformer output (now outputs properly masked probabilities) and fixed training loop for sorting test environment

irlco/pointer_transformer.py
@@ -6,13 +6,17 @@ from typing import Optional, Callable
 
 
 class PointerTransformerDecoderLayer(nn.Module):
+    """
+    Final decoder layer in the pointer transformer. Differs from a regular transformer decoder layer only slightly -
+    instead of 
+    """
     def __init__(self, d_model: int, nhead: int, dim_feedforward: int = 2048, dropout: float = 0.1,
                  activation: str = "relu"):
         super(PointerTransformerDecoderLayer, self).__init__()
         self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
         self.multihead_attn = nn.MultiheadAttention(d_model, nhead, dropout)
         self.logit = nn.Linear(d_model, 1)
-        self.softmax = nn.Softmax(dim=0)
+        self.softmax = nn.Softmax(dim=1)
 
         self.dropout = nn.Dropout(dropout)
         self.norm = nn.LayerNorm(d_model)
@@ -23,9 +27,11 @@ class PointerTransformerDecoderLayer(nn.Module):
         tgt2 = self.self_attn(tgt, tgt, tgt, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)[0]
         tgt = tgt + self.dropout(tgt2)
         tgt = self.norm(tgt)
-        tgt2 = self.multihead_attn(tgt, memory, memory, attn_mask=memory_mask, key_padding_mask=memory_key_padding_mask)[0]
-        logits = self.logit(tgt2)
-        probs = self.softmax(logits)
+        # Instead of returning the attention-weighted sum, return the weights over the source sequence for the last
+        # element of the target sequence
+        weights = self.multihead_attn(tgt, memory, memory, attn_mask=memory_mask, key_padding_mask=memory_key_padding_mask)[1][:,-1,:]
+        # Treat weights as logits since they don't actually sum to 1 - apply additive mask to logits
+        probs = self.softmax(weights + memory_mask[-1, :])
 
         return probs
 

irlco/sorting/train.py
@@ -1,31 +1,94 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 import irlco.sorting
 import irlco.pointer_transformer as pt
 
+# Constants for dimensions of transformer embedding dim. and number of heads for multihead attention
+EMBEDDING_DIM = 16
+NB_HEADS = 4
+assert EMBEDDING_DIM % NB_HEADS == 0, "Embedding dim must be divisible by the number of heads"
+
 # Initialise sequence sampler and reward model
 sequence_sampler = irlco.sorting.uniform_sampler_generator(3, 8, 0.0, 10.0)
-reward_model = irlco.sorting.sorted_terminal_reward
+reward_model = irlco.sorting.stepwise_reward
 
 # Initialise environment that samples initial states using sequence_sampler and returns reward from states
 # according to reward_model
 env = irlco.sorting.SortingEnv(reward_model, sequence_sampler)
 
 # Initialise pointer transformer policy network
-policy = pt.PointerTransformer(32, 1, 3, 3, 64)
+policy = pt.PointerTransformer(d_model=EMBEDDING_DIM, nhead=NB_HEADS, num_encoder_layers=3, num_decoder_layers=3,
+                               dim_feedforward=64)
 
 # Number of episodes to train for
-nb_episodes = 10_000
+nb_episodes = 20_000
 
 # Gradient optimizer
 optimizer = torch.optim.SGD(policy.parameters(), lr=0.001, momentum=0.9)
+
+
+# Mask generator (code taken from https://pytorch.org/tutorials/beginner/transformer_tutorial.html)
+def generate_square_subsequent_mask(size: int) -> torch.Tensor:
+    """
+    Generates a mask that prevents actions attending to subsequent actions in the transformer decoder.
+    Args:
+        size: Size of the mask (i.e. length of the solution so far.)
+
+    Returns:
+        Mask for transformer decoder.
+
+    """
+    mask = (torch.triu(torch.ones(size, size)) == 1).transpose(0, 1)
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    return mask
+
+
+def generate_sorted_element_mask(previous_actions, input_length: int) -> torch.Tensor:
+    """
+    Generates a mask that prevents actions from attending to elements of the unordered set that have already been
+    placed into the ordered sequence.
+    Args:
+        previous_actions: List of previous actions (in order) that we need to mask
+        input_length: Number of elements in the unordered sequence
+
+    Returns:
+        Memory mask of shape (nb of previous actions + 1, input sequence length) suitable for use in transformer
+
+    """
+    # Generate lower triangular matrix (creates columns for masked input elements)
+    # i_th column of masked_cols is equal to the {a_i}'th column of the mask
+    masked_cols = torch.tril(torch.ones(len(previous_actions) + 1, len(previous_actions)) * float('-inf'), diagonal=-1)
+    # Create empty mask
+    mask = torch.zeros(len(previous_actions) + 1, input_length)
+    # For each previous action, prevent further actions from attending to its corresponding input element
+    mask[:, previous_actions] = masked_cols
+    return mask
+
+
+# Input embedding: just use a random linear map R -> R^{EMBEDDING_DIM}
+embedding_weights = torch.rand(EMBEDDING_DIM) - 0.5
+
+
+def sorting_input_embedding(input: torch.Tensor) -> torch.Tensor:
+    """
+    Maps the sequence into a higher-dimensional vector space. The mapping is performed entrywise on the sequence.
+    Args:
+        input: The sequence we want to send to the higher-dimensional space. Dimension: (N).
+
+    Returns:
+        The image of the sequence in the higher-dimensional space. Dimension: (N, EMBEDDING_DIM).
+    """
+    return torch.einsum('i,j->ij', input, embedding_weights)
 
 # State & action representation:
 # ([x_1, ..., x_n], [a_1, ..., a_m]) where m <= n
 # [x_1, ..., x_n] is the unordered sequence (we want to learn how to sort this!)
 # a_1, ..., a_m describes our partial solution up to the m_th step - a_i is the index of the entry in the unordered
 # sequence which should be at position i in the ordered sequence
+
+epoch_returns = np.zeros(nb_episodes)
 
 for episode in range(nb_episodes):
     # Reset the episode
@@ -42,16 +105,28 @@ for episode in range(nb_episodes):
 
         # Calculate decoder input by fetching the entries in the unordered sequence that are indexed by the action ids
         # (i.e. from a sequence of indices, get the corresponding items)
-        partial_ordered_seq = torch.tensor([unordered_seq[action] for action in actions])
+        # Append 0 to start of sequence (represents context node)
+        partial_ordered_seq = torch.tensor([0.0] + [unordered_seq[action] for action in actions])
+
+        # Apply embedding to inputs
+        embedded_unordered_seq = sorting_input_embedding(unordered_seq)
+        embedded_partial_ordered_seq = sorting_input_embedding(partial_ordered_seq)
+
+        # Reshape inputs to add extra dimension for batch indexing - this is to match the shape of the transformer input
+        embedded_unordered_seq = embedded_unordered_seq.unsqueeze(1)
+        embedded_partial_ordered_seq = embedded_partial_ordered_seq.unsqueeze(1)
+
+        # Generate masks:
+        tgt_mask = generate_square_subsequent_mask(len(actions) + 1) # Stops actions attending to future actions
+        memory_mask = generate_sorted_element_mask(actions, len(unordered_seq)) # Stops actions attending to elements
+        # that have already been sorted
 
         # Get action probabilities from the policy
-        # TODO: add masking for actions to pointer transformer
-        print(unordered_seq)
-        print(partial_ordered_seq)
-        action_probs = policy.forward(unordered_seq, partial_ordered_seq)
+        action_probs = policy.forward(embedded_unordered_seq, embedded_partial_ordered_seq, tgt_mask=tgt_mask,
+                                      memory_mask=memory_mask)[0]
 
         # Sample action according to action_probs
-        action = np.random.choice(len(action_probs), p=action_probs)
+        action = np.random.choice(action_probs.numel(), p=action_probs.detach().numpy().flatten())
 
         # Take step in environment
         next_state, reward, _ = env.step(action)
@@ -65,7 +140,11 @@ for episode in range(nb_episodes):
         state = next_state
 
     # Calculate episode returns (no time decay)
+    # the conjugation by torch.flip is because we want to cumulatively sum episode_rewards backwards.
     episode_returns = torch.flip(torch.cumsum(torch.flip(episode_rewards, [0]), 0), [0])
+
+    # Store entire episode return for validation purposes
+    epoch_returns[episode] = episode_returns[0]
 
     # Calculate policy loss and gradients
     loss = torch.dot(episode_energies, episode_returns)
@@ -74,6 +153,12 @@ for episode in range(nb_episodes):
     # Take gradient step
     optimizer.step()
 
+    # Print progress once every 100 episodes.
+    if (episode + 1) % 100 == 0:
+        print("Completed episode {}/{}.".format(episode+1, nb_episodes))
+
+plt.plot(np.cumsum(epoch_returns))
+plt.savefig('sorting_epoch_returns.png')
 
 
 